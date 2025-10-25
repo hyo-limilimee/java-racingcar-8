a27a5dd6beb2315db021bc143d85dc24dc8d1220
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-10-26 01:29:54 +0900

    feat: make car name input sequence

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -8,8 +8,12 @@ import racingcar.utils.InputValidator;
 public class InputView {
 
     public List<String> readCarNames() {
-    }
-
-    public int readTryCount() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        InputValidator.validateNotEmpty(input);
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .toList();
     }
 }
