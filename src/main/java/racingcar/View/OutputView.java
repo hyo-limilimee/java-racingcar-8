d705eda65a7c7d12787ef8c35569fe6ae144bb85
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-10-26 12:13:18 +0900

    feat: making output view logic

---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,26 @@
+package racingcar.View;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void printResultTitle() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public void printRound(List<String> results) {
+        for (String r : results) {
+            System.out.println(r);
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+    public void printError(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+}
