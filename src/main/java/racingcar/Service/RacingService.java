49b4c449cd98400e0c6edb54e9180448a5c80408
HyoRim Kang <80194553+hyo-limilimee@users.noreply.github.com>
2025-10-26 10:33:03 +0900

    feat: make rendering cars sequence

---
+++ src/main/java/racingcar/Service/RacingService.java
@@ -0,0 +1,31 @@
+package racingcar.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
+
+public class RacingService {
+
+    private final RacingGame game;
+
+    public RacingService(List<String> names) {
+        RacingValidator.validateNames(names);
+        this.game = new RacingGame(new Cars(names));
+    }
+
+    public void playOnce() {
+        game.playOnce();
+    }
+
+    public List<String> renderCars() {
+        return game.getCars().getAll()
+                .stream()
+                .map(Car::renderPosition)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getWinnerNames() {
+    }
+}
